c09d46fa61c239224552abe143cce44192ef6ace
Peter Kang <peterjkang@gmail.com>
2016-03-02 17:29:23 -0500

    Add dummy POST routes
    Add POST routes for /where, /at, and /account/new and create new
    controller for POST routes

---
+++ app/controllers/Whereuat.scala
@@ -0,0 +1,21 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import com.mongodb.casbah.Imports._
+
+class Whereuat extends Controller {
+
+  def createAccount = Action {
+    Ok("Create account placeholder")
+  }
+
+  def request = Action {
+    Ok("Request placeholder")
+  }
+
+  def respond = Action {
+    Ok("Response placeholder")
+  }
+
+}
