5f3389d5892530375d2b9f1ad393b445c566313a
Peter Kang <peterjkang@gmail.com>
2016-03-04 02:27:31 -0500

    Create reference routes
    Set up reference routes and a Reference controller as a place to put
    actions that are only used as a reference for functionality later down
    the line. Moved all of the mongo query code to this new controller.

---
+++ app/controllers/Reference.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import com.mongodb.casbah.Imports._
+import com.mongodb.util.JSON._
+
+class Reference extends Controller {
+  // Database Query Reference route
+  val dbQuery = Action {
+    val mongoClient = MongoClient("localhost", 27017)
+    val db = mongoClient("test")
+    val coll = db("test")
+
+    val docs = coll.find()
+    val list = docs.toList
+    
+    Ok(s"Test collection query:\n${serialize(list)}")
+  }
+}
