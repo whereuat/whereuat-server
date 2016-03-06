66549980de712c8a71dcfa866b3ed3230936229b
Peter Kang <peterjkang@gmail.com>
2016-03-06 17:00:56 -0500

    Comment in Global placeholder class

--- app/Global.scala
+++ app/Global.scala
@@ -3,6 +3,8 @@ import play.api._
 import utils.Config
 
 object Global extends GlobalSettings {
+  // Global.scala normally overrides default Play framework behavior
+  // with this class
 }
 
 package object global {
